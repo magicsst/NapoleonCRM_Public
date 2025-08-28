5feaee5c0ea00b5a3282972bd7430479979c12dc
Pantazis Siklarlis <46692568+magicsst@users.noreply.github.com>
2025-08-28 10:00:33 +0300

    Add CRUD Blazor pages for orders

---
+++ NapoleonCRM.Shared/Models/Order.cs
@@ -0,0 +1,42 @@
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+
+namespace NapoleonCRM.Shared.Models;
+
+[DataContract]
+public class Order
+{
+    [Key]
+    [DataMember]
+    public long? Id { get; set; }
+
+    [DataMember]
+    public string? ProductId { get; set; }
+
+    [DataMember]
+    public string? ServiceId { get; set; }
+
+    [DataMember]
+    public long? CustomerId { get; set; }
+
+    [DataMember]
+    public long? Quantity { get; set; }
+
+    [DataMember]
+    public decimal? TotalAmount { get; set; }
+
+    [DataMember]
+    public DateTime? OrderDate { get; set; }
+
+    [DataMember]
+    public string? ReceiptPhoto { get; set; }
+
+    [DataMember]
+    public string? Notes { get; set; }
+
+    [DataMember]
+    public DateTime? CreatedDate { get; set; }
+
+    [DataMember]
+    public DateTime ModifiedDate { get; set; }
+}
